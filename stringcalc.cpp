8418e666d932d70aaa81c3080aac2a27034bafcf
sushu2001 <sushantawathare212@gmail.com>
2022-09-07 22:52:49 +0530

    first commit

---
+++ stringcalc.cpp
@@ -0,0 +1,89 @@
+/*  Name: Sushant Sudhakar Awathare  
+    Date: 07/09/2022 */
+
+/*************************** STRING CALCULATOR ***************************/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class StringCalculator{
+private:
+    string numbers;
+public:
+    StringCalculator(){}
+    StringCalculator(string n){             // Initialization of parameterized constructor
+        this->numbers = n;                  // This pointer is used to store the address of its self
+    }
+    int add(string numbers){
+        int stringLength = numbers.size();  // Declaration of length of the string by using size() function
+        int addition = 0;                   // Declaration of addition of numbers in the string
+        string tempAnswer = "";
+
+        bool ifNegative = false; 
+        bool nextLine = false, Negative = false;
+        vector<int> numberOfNegatives;
+
+        for(int i=0; i < stringLength; i++){
+            char ch = numbers[i];
+            
+            if(isdigit(ch)){                // To check whether character is  a digits (0 to 9) or not
+                tempAnswer += ch;
+            }
+            else if(isalpha(ch)){           // To check whether character is  a alphaNumneric (lowecase & uppercase) or not
+                int temp = ch - 'a' + 1;
+                tempAnswer += to_string(temp);
+            }
+            else if((int)ch == 10 || ch==';'){
+                nextLine = true;
+            }
+            else if(ch == '-'){
+                ifNegative = true;
+                Negative = true;
+                continue;
+            }
+            else if(ch != ','){
+                continue;
+            }
+
+            if((ch == ',' || i == stringLength-1 || nextLine) && tempAnswer.size() > 0){
+                // cout << num << " ";
+                int number = stoi(tempAnswer);
+                tempAnswer = "";
+
+                if(Negative){                             // if the number is negative then push it into the vector
+                    numberOfNegatives.push_back(-number);
+                    Negative = false;
+                    continue;
+                }
+                nextLine = false;
+
+                if(number > 1000) continue;
+                addition += number;
+            }
+        }
+
+        if(ifNegative){                                   // to check negative numbers in the array and to throw an exception
+            for(auto &negNum : numberOfNegatives){
+                cout << negNum << " ";
+            }
+            cout << endl;
+            throw invalid_argument( "Result must not Contain negatives" );
+        }
+
+        return addition;           //Return the Result of Adfition of string
+
+    }
+};
+
+int main()
+{
+    StringCalculator sc;
+    int r = sc.add("");
+    int m = sc.add("1");
+    int x = sc.add("1,2");
+    cout << "The Output of Test Case 1: " << endl;
+    cout << "Input \"\": "<< r << endl;
+    cout << "Input \"1\": "<< m << endl;
+    cout << "Input \"1,2\": "<< x << endl;
+    return 0;
+}
